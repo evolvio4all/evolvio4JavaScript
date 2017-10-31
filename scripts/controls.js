abab506a95a18815caf3167cf056cf14038f1fae
UnknownEvil <reapor12@hotmail.com>
2017-10-31 09:30:06 -0400

    Corrected Zooming

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,95 +1,95 @@
 function keyEvents() {
-	if (keyDown(controls.slowDown)) {
-		timescale = 0.2;
-	} else if (keyDown(controls.fastForward)) {
-		timescale = 50;
-	} else if (keyDown(controls.stop)) {
-		timescale = 0;
-	} else {
-		timescale = 1;
-	}
+    if (keyDown(controls.slowDown)) {
+        timescale = 0.2;
+    } else if (keyDown(controls.fastForward)) {
+        timescale = 50;
+    } else if (keyDown(controls.stop)) {
+        timescale = 0;
+    } else {
+        timescale = 1;
+    }
 }
 
 let mouse = {
-	up: {},
-	down: {},
-	current: {},
-	isdown: false
+    up: {},
+    down: {},
+    current: {},
+    isdown: false
 };
 
 window.onmousedown = function(e) {
-	mouse.down.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
-	mouse.down.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
+    mouse.down.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
+    mouse.down.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
 
-	mouse.isdown = true;
+    mouse.isdown = true;
 
-	selectedCreature = null;
+    selectedCreature = null;
 
-	for (let creature of creatures) {
-		if (creature.select()) {
-			selectedCreature = creature;
-		}
-	}
+    for (let creature of creatures) {
+        if (creature.select()) {
+            selectedCreature = creature;
+        }
+    }
 };
 
 window.onmouseup = function(e) {
-	mouse.up.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
-	mouse.up.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
+    mouse.up.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
+    mouse.up.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
 
-	mouse.isdown = false;
+    mouse.isdown = false;
 };
 
 window.onmousemove = function(e) {
-	mouse.current.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
-	mouse.current.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
+    mouse.current.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
+    mouse.current.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
 
-	if (mouse.isdown) {
-		cropx += lcx - mouse.current.x;
-		cropy += lcy - mouse.current.y;
-	}
+    if (mouse.isdown) {
+        cropx += lcx - mouse.current.x;
+        cropy += lcy - mouse.current.y;
+    }
 
-	lcx = mouse.current.x;
-	lcy = mouse.current.y;
+    lcx = mouse.current.x;
+    lcy = mouse.current.y;
 };
 
 window.onmousewheel = function(e) {
-	mouse.current.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
-	mouse.current.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
+    viewport.width -= e.wheelDelta;
+    viewport.height -= e.wheelDelta * 0.5625;
 
-	viewport.width -= e.wheelDelta;
-	viewport.height -= e.wheelDelta * 0.5625;
+    if (viewport.width > minZoomLevel) {
+        viewport.width = minZoomLevel;
+        viewport.height = minZoomLevel * 0.5625;
+    }
 
-	if (viewport.width > minZoomLevel) {
-		viewport.width = minZoomLevel;
-		viewport.height = minZoomLevel * 0.5625;
-	}
+    if (viewport.width < maxZoomLevel) {
+        viewport.width = maxZoomLevel;
+        viewport.height = maxZoomLevel * 0.5625;
+    }
 
-	if (viewport.width < maxZoomLevel) {
-		viewport.width = maxZoomLevel;
-		viewport.height = maxZoomLevel * 0.5625;
-	}
-	
-	cropx += lcx - mouse.current.x;
-	cropy += lcy - mouse.current.y;
-	
-	lcx = mouse.current.x;
-	lcy = mouse.current.y;
+    mouse.current.x = (e.clientX - viewport.getBoundingClientRect().left) * (viewport.width / viewport.clientWidth);
+    mouse.current.y = (e.clientY - viewport.getBoundingClientRect().top) * (viewport.height / viewport.clientHeight);
+
+    cropx += lcx - mouse.current.x;
+    cropy += lcy - mouse.current.y;
+
+    lcx = mouse.current.x;
+    lcy = mouse.current.y;
 };
 
 window.onkeydown = function(e) {
-	activeKeys.push(e.keyCode);
+    activeKeys.push(e.keyCode);
 };
 
 window.onkeyup = function(e) {
-	var z = activeKeys.indexOf(e.keyCode);
-	for (i = activeKeys.length; i > 0; i--) {
-		if (z == -1) break;
-		activeKeys.splice(z, 1);
-		z = activeKeys.indexOf(e.keyCode);
-	}
+    var z = activeKeys.indexOf(e.keyCode);
+    for (i = activeKeys.length; i > 0; i--) {
+        if (z == -1) break;
+        activeKeys.splice(z, 1);
+        z = activeKeys.indexOf(e.keyCode);
+    }
 };
 
 function keyDown(key) {
-	if (activeKeys.indexOf(keys[key]) > -1) return true;
-	return false;
+    if (activeKeys.indexOf(keys[key]) > -1) return true;
+    return false;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,147 +1,150 @@
 function main() {
-    if (timescale >= 1) {
-        for (let ts = 0; ts < timescale; ts++) {
-            update();
-        }
-    } else {
-        tc++;
-        if (tc > 1 / timescale) {
-            update();
-            
-            tc = 0;
-        }
-    }
-    
-    keyEvents();
+	if (timescale >= 1) {
+		for (let ts = 0; ts < timescale; ts++) {
+			update();
+		}
+	} else {
+		tc++;
+		if (tc > 1 / timescale) {
+			update();
+
+			tc = 0;
+		}
+	}
+
+	keyEvents();
 }
 
-function wallLock(i) {
-    if (creatures[i].x <= 0) {
-        creatures[i].x = 0;
-    }
+function wallLock(creature) {
+	if (creature.x <= 0) {
+		creature.x = 0;
+	}
 
-    if (creatures[i].x >= display.width - 1) {
-        creatures[i].x = display.width - 1;
-    }
+	if (creature.x >= display.width - 1) {
+		creature.x = display.width - 1;
+	}
 
-    if (creatures[i].y <= 0) {
-        creatures[i].y = 0;
-    }
+	if (creature.y <= 0) {
+		creature.y = 0;
+	}
 
-    if (creatures[i].y >= display.height - 1) {
-        creatures[i].y = display.height - 1;
-    }
+	if (creature.y >= display.height - 1) {
+		creature.y = display.height - 1;
+	}
 }
 
-function clampSize(i) {
-    if (creatures[i].size > maxCreatureSize) creatures[i].size = maxCreatureSize;
-    if (creatures[i].size < minCreatureSize) {
-        if (creatures[i] == selectedCreature) selectedCreature = null;
-        creatures[i].die();
-    }
+function clampSize(creature) {
+	if (creature.size > maxCreatureSize) creature.size = maxCreatureSize;
+	if (creature.size < minCreatureSize) {
+		if (creature == selectedCreature) selectedCreature = null;
+		creature.die();
+	}
 }
 
 function update() {
-    tick++;
-    population = creatures.length;
+	tick++;
+	population = creatures.length;
 
-    for (let i in creatures) {
-        creatures[i].maxSpeed = maxCreatureSpeed;
-        wallLock(i);
+	for (let creature of creatures) {
+		creature.maxSpeed = maxCreatureSpeed;
+		wallLock(creature);
 
-        let x = creatures[i].x / display.width;
-        let y = creatures[i].y / display.height;
-        let size = (creatures[i].size - minCreatureSize) / (maxCreatureSize - minCreatureSize);
-        let pos = creatures[i].getPosition();
-        let tileFood = map[pos[0]][pos[1]].food / maxTileFood;
-        let eatPower = creatures[i].output[2];
-        let age = creatures[i].age / reproduceAge;
-        let reproduceTime = creatures[i].reproduceTime / minReproduceTime;
-        let xMove = creatures[i].output[0];
-        let yMove = creatures[i].output[1];
+		let x = creature.x / display.width;
+		let y = creature.y / display.height;
+		let size = (creature.size - minCreatureSize) / (maxCreatureSize - minCreatureSize);
+		let pos = creature.getPosition();
+		let tileFood = map[pos[0]][pos[1]].food / maxTileFood;
+		let eatPower = creature.output[2];
+		let age = creature.age / reproduceAge;
+		let reproduceTime = creature.reproduceTime / minReproduceTime;
+		let xMove = creature.output[0];
+		let yMove = creature.output[1];
 
-        creatures[i].input = [x, y, size, tileFood, eatPower, age, reproduceTime, xMove, yMove];
-        creatures[i].output = creatures[i].feedForward(creatures[i].input);
+		creature.input = [x, y, size, tileFood, eatPower, age, reproduceTime, xMove, yMove];
+		creature.output = creature.feedForward(creature.input);
 
-        creatures[i].move();
-        creatures[i].metabolize();
+		creature.move();
+		creature.metabolize();
 
-        if (creatures[i].output[2] > 0) creatures[i].eat(pos);
-        if (creatures[i].output[3] > 0) {
-            creatures[i].reproduce();
-        }
+		if (creature.output[2] > 0) creature.eat(pos);
+		if (creature.output[3] > 0) {
+			creature.reproduce();
+		}
 
-        creatures[i].tick();
+		creature.tick();
 
-        clampSize(i);
-    }
+		clampSize(creature);
+		
+		if (creature == selectedCreature) {
+			cropx -= (cropx - (creature.x - viewport.width / 2)) / 50;
+			cropy -= (cropy - (creature.y - viewport.height / 2)) / 50;
+		}
+	}
 
-    for (let i in map) {
-        for (let j in map[i]) {
-            if (map[i][j].type == 1) {
-                if (map[i][j].food < maxTileFood) map[i][j].food += foodRegrowRate;
-            }
-        }
-    }
+	for (let i in map) {
+		for (let j in map[i]) {
+			if (map[i][j].type == 1) {
+				if (map[i][j].food < maxTileFood) map[i][j].food += foodRegrowRate;
+			}
+		}
+	}
 }
 
 function render() {
-    ctx.clearRect(0, 0, display.width, display.height);
-    ctz.clearRect(0, 0, viewport.width, viewport.height);
+	ctx.clearRect(0, 0, display.width, display.height);
+	ctz.clearRect(0, 0, viewport.width, viewport.height);
 
-    ctx.fillStyle = "#1799B5";
-    ctx.fillRect(0, 0, display.width, display.height);
+	ctx.fillStyle = "#1799B5";
+	ctx.fillRect(0, 0, display.width, display.height);
 
-    for (let i in map) {
-        for (let j in map[i]) {
-            if (map[i][j].type === 0) continue;
-            ctx.fillStyle = "hsl(90," + Math.round(map[i][j].food / maxTileFood * 100) + "%, 40%)";
-            ctx.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
-        }
-    }
+	for (let i in map) {
+		for (let j in map[i]) {
+			if (map[i][j].type === 0) continue;
+			ctx.fillStyle = "hsl(90," + Math.round(map[i][j].food / maxTileFood * 100) + "%, 40%)";
+			ctx.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
+		}
+	}
 
-    ctx.lineWidth = 5;
-    for (let creature of creatures) {
-        ctx.strokeStyle = "#ffffff";
-        if (selectedCreature == creature) {
-            ctx.strokeStyle = "#ff0000";
-            cropx -= (cropx - (creature.x - viewport.width / 2)) / 50;
-            cropy -= (cropy - (creature.y - viewport.height / 2)) / 50;
-        }
+	ctx.lineWidth = 5;
+	for (let creature of creatures) {
+		ctx.strokeStyle = "#ffffff";
+		if (selectedCreature == creature) {
+			ctx.strokeStyle = "#ff0000";
+		}
 
-        ctx.fillStyle = creature.color;
-        ctx.fillCircle(creature.x, creature.y, creature.size, true);
-    }
+		ctx.fillStyle = creature.color;
+		ctx.fillCircle(creature.x, creature.y, creature.size, true);
+	}
 
-    ctz.drawImage(display, cropx, cropy, viewport.width, viewport.height, 0, 0, viewport.width, viewport.height);
-    ctz.lineWidth = 5;
-    if (selectedCreature !== null) {
-        ctz.beginPath();
-        for (let i = 0; i < layers.length; i++) {
-            for (let j = 0; j < layers[i]; j++) {
-                for (let k = 0; k < layers[i - 1]; k++) {
-                    //ctz.strokeStyle = "hsl(0, 0%, " + Math.floor(creatures[selectedCreature].network.axons[i - 1][j][k] * 100) + "%)";
-                    ctz.moveTo((i - 1) * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset, k * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset);
-                    ctz.lineTo(i * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset, j * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset);
-                }
-            }
-        }
-        ctz.stroke();
+	ctz.drawImage(display, cropx, cropy, viewport.width, viewport.height, 0, 0, viewport.width, viewport.height);
+	ctz.lineWidth = 5;
+	if (selectedCreature !== null) {
+		ctz.beginPath();
+		for (let i = 0; i < layers.length; i++) {
+			for (let j = 0; j < layers[i]; j++) {
+				for (let k = 0; k < layers[i - 1]; k++) {
+					//ctz.strokeStyle = "hsl(0, 0%, " + Math.floor(creatures[selectedCreature].network.axons[i - 1][j][k] * 100) + "%)";
+					ctz.moveTo((i - 1) * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset, k * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset);
+					ctz.lineTo(i * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset, j * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset);
+				}
+			}
+		}
+		ctz.stroke();
 
-        ctz.fillStyle = "#222222";
-        ctz.font = "24px Calibri";
-        for (let i = 0; i < layers.length; i++) {
-            for (let j = 0; j < layers[i]; j++) {
-                ctz.fillCircle(i * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset, j * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset, nnui.size, nnui.stroke);
-            }
-        }
+		ctz.fillStyle = "#222222";
+		ctz.font = "24px Calibri";
+		for (let i = 0; i < layers.length; i++) {
+			for (let j = 0; j < layers[i]; j++) {
+				ctz.fillCircle(i * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset, j * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset, nnui.size, nnui.stroke);
+			}
+		}
 
-        ctz.fillStyle = "#ffffff";
-        for (let i = 0; i < layers.length; i++) {
-            for (let j = 0; j < layers[i]; j++) {
-                ctz.fillText(selectedCreature.network.neurons[i][j].toFixed(2), i * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset - 20, j * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset + 10);
-            }
-        }
-    }
-    requestAnimationFrame(render);
+		ctz.fillStyle = "#ffffff";
+		for (let i = 0; i < layers.length; i++) {
+			for (let j = 0; j < layers[i]; j++) {
+				ctz.fillText(selectedCreature.network.neurons[i][j].toFixed(2), i * (nnui.size * 2 + nnui.xspacing) + nnui.xoffset - 20, j * (nnui.size * 2 + nnui.yspacing) + nnui.yoffset + 10);
+			}
+		}
+	}
+	requestAnimationFrame(render);
 }
