22d9a9fc21459408b462c380080a3025e1f92b9b
UnknownEvil <reapor12@hotmail.com>
2017-11-01 19:39:38 -0400

    Delete init.js

--- init.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < minCreatures; i++) {
-	creatures.push(new Creature(seededNoise() * (mapSize * tileSize), seededNoise() * (mapSize * tileSize), seededNoise() * maxCreatureSize + minCreatureSize));
-}
-
-generateMap();
-
-setInterval(main, 1000 / 60);
-requestAnimationFrame(render);
