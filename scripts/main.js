0d6dcf02b3e243b4b929d8521332f3492d8d9e3e
winand appels <winnie33winnie33@gmail.com>
2018-10-14 00:56:30 +0200

    merge

--- scripts/main.js
+++ scripts/main.js
@@ -251,7 +251,7 @@ function render() {
 		let tilex = Math.floor((mouse.current.x + cropx) / tileSize / zoomLevel);
 		let tiley = Math.floor((mouse.current.y + cropy) / tileSize / zoomLevel);
 		ctz.strokeText(map[tilex][tiley].food.toFixed(1), tilex * tileSize * zoomLevel - cropx + tileSize / 2 * zoomLevel, tiley * tileSize * zoomLevel - cropy + tileSize / 1.5 * zoomLevel);
-		//ctz.fillText(map[tilex][tiley].food.toFixed(1), tilex * tileSize * zoomLevel - cropx + tileSize / 2 * zoomLevel, tiley * tileSize * zoomLevel - cropy + tileSize / 2 * zoomLevel);
+		ctz.fillText(map[tilex][tiley].food.toFixed(1), tilex * tileSize * zoomLevel - cropx + tileSize / 2 * zoomLevel, tiley * tileSize * zoomLevel - cropy + tileSize / 1.5 * zoomLevel);
 
 		ctx.strokeStyle = "#ffffff";
 		ctx.lineWidth = 1;
